690ba3e38a0dd9076f5ced874b43879dc26e4591
Shanmukha Sarma Bankupalli <bshanmukhasarma@gmail.com>
2025-07-10 16:08:08 +0530

    Add files via upload

--- csort.py
+++ csort.py
@@ -1,32 +1,13 @@
-#couting sort 
-def csort(arr):
-    if not arr:
-        return[]
-    max_val=max(arr)
-    count=[0]*(max_val+1)
-    # freq
+def csortString(s):
+    count=[0]*26
+    for char in s:
+        count[ord(char)-ord('a')]+=1
+    sorted_str=" "
+    for i in range(26):
+        sorted_str+=chr(i+ord('a'))*count[i]
+    return sorted_str
 
-    for num in arr:
-        count[num]+=1
-    #count
-
-    for i in range(1, len(count)):
-        count[i]+=count[i-1]
-    output=[0]*len(arr)
-    # stable sort
-    for num in reversed(arr):
-        output[count[num]-1]=num
-        count[num]-=1
-    #coping sorted list
-
-    for i in range(len(arr)):
-        arr[i]=output[i]
-size=int(input("Enter the elements:"))
-arr=[]
-print("Enter the Elements:")
-for _ in range(size):
-    num=int(input())
-    arr.append(num)
-print("before:",arr)
-csort(arr)
-print("After:",arr)
+name=input("Enter a single word:")
+sorted_name=csortString(name)
+print("Original string:",name)
+print("Sorted string:",sorted_name)
